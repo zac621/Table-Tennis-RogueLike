0107a6b03f744ab94b99f759ef5fbd4fd651b64d
zac621 <zac@falez.ca>
2026-08-09 21:57:29 -0700

    Refine websocket client connection logic

--- api/ws.ts
+++ api/ws.ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { createWebSocketServer } from "../backend/ws-manager";
 
+export const config = {
+  runtime: "nodejs",
+};
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
   createWebSocketServer(req, res);
 }
