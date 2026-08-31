5ee1be2af4e86153b7fbf3bccc7d006a3eca3d4c
zac621 <zac@falez.ca>
2026-08-31 02:49:07 -0700

    Crd height change 3

--- src/pages/UpgradeDraft.tsx
+++ src/pages/UpgradeDraft.tsx
@@ -41,7 +41,7 @@ export default function UpgradeDraft({ pool, draftTurn, p1Name, p2Name, myPlayer
                 key={`${u.id}-${i}`}
               >
                 <Card
-                  className={`relative w-56 h-35 flex flex-col p-4 ${disabled ? 'opacity-60 cursor-not-allowed' : 'cursor-pointer hover:border-primary'} transition-colors bg-card border-border glow-${u.rarity}`}
+                  className={`relative w-86 h-35 flex flex-col p-4 ${disabled ? 'opacity-60 cursor-not-allowed' : 'cursor-pointer hover:border-primary'} transition-colors bg-card border-border glow-${u.rarity}`}
                   onClick={() => !disabled && onSelect(u, i)}
                   data-testid={`card-upgrade-${u.id}`}
                 >
